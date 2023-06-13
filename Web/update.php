b81f1b00ebab51e2a2cf28fd5d78b474c7a8147c
NumberMcj-arts <numbermcj@googlemail.com>
2023-06-13 14:35:20 +0200

    first try loading json file

--- Web/update.php
+++ Web/update.php
@@ -1,13 +1,16 @@
 <?php
 	//echo 'zack</br>';
-	$argument = $_POST['val'];
+	$path = "/home/debian/database/sample.json";
+	$json_str = file_get_contents($path);
+	$json_data = json_decode($json_str, true);
+	//$argument = $_POST['val'];
 	// $_POST['b'];
-	$path = '/home/debian/Prog_austoben/KI-Projekte/UNO_AI_Web/';
-	$cmd = 'test3.py';
-	$total_cmd = 'python ' . $path . $cmd;
+	//$path = '/home/debian/Prog_austoben/KI-Projekte/UNO_AI_Web/';
+	//$cmd = 'test3.py';
+	//$total_cmd = 'python ' . $path . $cmd;
 	
 	//echo $total_cmd . '</br>';
 	
-	$result = shell_exec($total_cmd);
-	echo $result . "Fettsack";
+	//$result = shell_exec($total_cmd);
+	echo "Viech";
 ?>
