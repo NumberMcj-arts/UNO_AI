8d629e4f9fe8e12668ee7596b9d7cd0dbd3d28c0
NumberMcj-arts <numbermcj@gmail.com>
2023-06-13 14:35:39 +0000

    reading JSON works - cleaned up a bit

--- Web/update.js
+++ Web/update.js
@@ -1,13 +1,5 @@
 
 function update(){
-//	$.ajax({
-//		url: "update.php",
-//		success: function(state){
-//			var nr = state;
-//			document.getElementById('state').value = nr;
-//		};
-//	})
-	
 	$.ajax({
 		type: "POST",
 		url: "update.php",
@@ -17,29 +9,12 @@ function update(){
 //		}
 	}).done(function(o) {
 		const game_state = JSON.parse(o);
-		//const test = JSON.parse('{"a": 13, "b": 5}')
 		document.getElementById('kack').value = game_state.card_on_top;
-		//alert(game_state.current_player)
-		//alert("Test")
 	});
 }
 
 function initState(){
-	//const test = JSON.parse('{"a": 13, "b": 5, "c": 10}')
-	//document.getElementById('kack').value = test.c;
-	$.ajax({
-		type: "POST",
-		url: "update.php",
-//		data: {
-			val: document.getElementById('state').value//,
-//			b: 'zack'
-//		}
-	}).done(function(o) {
-		const game_state = JSON.parse(o);
-		//const test = JSON.parse('{"a": 13, "b": 5}')
-		document.getElementById('state').value = game_state.current_player;
-		//alert(game_state.current_player)
-		//alert("Test")
-	});
+	const test = JSON.parse('{"a": 13, "b": 5, "c": 10}')
+	document.getElementById('kack').value = test.c;
 }
 
